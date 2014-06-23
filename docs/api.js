24bb430a823002821233228d56e59592e5729280
Drew Ewing <drew@phenocode.com>
2014-06-23 01:17:37 -0700

    Adding docs to repo

---
+++ docs/api.js
@@ -0,0 +1,19 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "OOP",
+        "OOP.Class",
+        "OOP.Object"
+    ],
+    "modules": [
+        "OOP"
+    ],
+    "allModules": [
+        {
+            "displayName": "OOP",
+            "name": "OOP",
+            "description": "oop.js - Pseudo-object-oriented library for JavaScript"
+        }
+    ]
+} };
+});
